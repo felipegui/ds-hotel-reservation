681651bb2b5774458e75ed2b653f8d6afe213d2a
Felipe Gui <fguiengenharia@gmail.com>
2023-11-30 16:15:02 -0300

    Versão ruim (delegação de verificação a classe reservation)

--- ds-hotel-reservation/src/application/Program.java
+++ ds-hotel-reservation/src/application/Program.java
@@ -41,18 +41,12 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Erro in reservation: Reservation dates for update must be future dates.");
-			} else if (!checkOut.after(checkIn)) {
-				System.out.println("Erro in reservation: Check-out date must be after check-in date.");
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println(error);
 			} else {
-				// Método que atualiza as datas
-				reservation.updateDates(checkIn, checkOut);
-
 				System.out.println("Reservation: " + reservation);
 			}
-
 		}
 
 		sc.close();
--- ds-hotel-reservation/src/model/entities/Reservation.java
+++ ds-hotel-reservation/src/model/entities/Reservation.java
@@ -9,9 +9,9 @@ public class Reservation {
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	public Reservation() {
 	}
 
@@ -36,20 +36,31 @@ public class Reservation {
 	public Date getCheckOut() {
 		return checkOut;
 	}
-	
+
 	public long duration() {
-		//Obter a diferença em milissegundos
+		// Obter a diferença em milissegundos
 		long diff = checkOut.getTime() - checkIn.getTime();
-		
-		//Converter para dias
+
+		// Converter para dias
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	public void updateDates(Date checkIn, Date checkOut) {
+
+	public String updateDates(Date checkIn, Date checkOut) {
+
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Erro in reservation: Reservation dates for update must be future dates.";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Erro in reservation: Check-out date must be after check-in date.";
+		}
+
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+
+		return null;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut)
