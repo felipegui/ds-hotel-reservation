17524ff9110bed196d05f407d43496bc4909bc09
Felipe Gui <fguiengenharia@gmail.com>
2023-11-30 22:30:00 -0300

    Versão boa, parte I - (Usando tratamento de exceção)

--- ds-hotel-reservation/src/model/entities/Reservation.java
+++ ds-hotel-reservation/src/model/entities/Reservation.java
@@ -45,20 +45,19 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) {
 
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Erro in reservation: Reservation dates for update must be future dates.";
+			throw new IllegalArgumentException(
+					"Erro in reservation: Reservation dates for update must be future dates.");
 		}
 		if (!checkOut.after(checkIn)) {
-			return "Erro in reservation: Check-out date must be after check-in date.";
+			throw new IllegalArgumentException("Erro in reservation: Check-out date must be after check-in date.");
 		}
 
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-
-		return null;
 	}
 
 	@Override
